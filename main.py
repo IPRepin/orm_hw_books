0ff411f0fedfab3b100e270ede24e56797ed8e94
Pavel Repin <sghcd2012@yandex.ru>
2022-11-17 12:07:53 +0300

    add class tables

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 
 from config import login, password, conn
+from models import create_tables
 
 '''создаем подключение'''
 DSN = f'postgresql://{login}:{password}@{conn}/books_db'
 engine = sqlalchemy.create_engine(DSN)
-# create_tables(engine)
+create_tables(engine)
 
 '''Создаем сессию'''
 Session = sessionmaker(bind=engine) #переменная для создания сессий
---
+++ models.py
@@ -0,0 +1,68 @@
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+
+'''создаем классы таблиц'''
+class Publisher(Base):
+    __tablename__ = "publisher"
+    publisher_id = sq.Column(sq.INTEGER, primary_key=True)
+    publisher_name = sq.Column(sq.char(length=48), unique=True)
+
+    '''Переопределяем метод str для понятного отображения таблицы в консоли'''
+    def __str__(self):
+        return f'{self.publisher_id}, {self.publisher_name}'
+
+class Book(Base):
+    __tablename__ = "book"
+    book_id = sq.Column(sq.INTEGER, primary_key=True)
+    book_name = sq.Column(sq.char(length=100), unique=True)
+    id_publisher = sq.Column(sq.INTEGER, sq.ForeignKey('publisher_id'), nullable=False)
+
+    publisher = relationship(Publisher, backref="book")
+
+    '''Переопределяем метод str для понятного отображения таблицы в консоли'''
+    def __str__(self):
+        return f'{self.id_publisher}: ({self.book_id}, {self.book_name})'
+
+class Shop(Base):
+    __tablename__ = "shop"
+    shop_id = sq.Column(sq.INTEGER, primary_key=True)
+    shop_name = sq.Column(sq.char(length=100), unique=True)
+
+    '''Переопределяем метод str для понятного отображения таблицы в консоли'''
+    def __str__(self):
+        return f'{self.shop_id}, {self.shop_name}'
+
+
+
+class Stock(Base):
+    __tablename__ = "stock"
+    stock_id = sq.Column(sq.INTEGER, primary_key=True)
+    id_book = sq.Column(sq.INTEGER, sq.ForeignKey('book_id'), nullable=False)
+    id_shop = sq.Column(sq.INTEGER, sq.ForeignKey('shop_id'), nullable=False)
+    count = sq.Column(sq.INTEGER, nullable=False)
+
+    shop = relationship(Shop, backref="shop_id")
+    book = relationship(Book, backref="book_id")
+
+    def __str__(self):
+        return f'{self.stock_id}: ({self.id_book}, {self.id_shop}, {self.count})'
+
+class Sale(Base):
+    __tablename__ = "sale"
+    sale_id = sq.Column(sq.INTEGER, primary_key=True)
+    prise = sq.Column(sq.INTEGER, nullable=False)
+    data_sale = sq.Column(sq.date, nullable=False)
+    id_stock = sq.Column(sq.INTEGER, sq.ForeignKey('stock_id'), nullable=False)
+    count = sq.Column(sq.INTEGER, nullable=False)
+
+    stock = relationship(Stock, backref="stock_id")
+
+    def __str__(self):
+        return f'{self.stock_id}: ({self.id_book}, {self.id_shop}, {self.count})'
+
+'''Функция создания таблиц'''
+def create_tables(engine):
+    Base.metadata.create_all(engine)
